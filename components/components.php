51c44711699e62384feae8df42c5d71d8f789f26
komugikotan <komugikotan1@gmail.com>
2023-06-07 02:36:48 +0900

    Wikipedia対応・リーダー表示の検索ボックスの対応・GIFへの緊急対応

--- components/components.php
+++ components/components.php
@@ -32,12 +32,21 @@ function Add_Prefix_2_Img_Src($content, $prefix){
 // URLが画像ファイルかどうかを正規表現で確認する関数
 function is_image_url($url) {
     // 画像ファイルの拡張子を表す正規表現
-    $pattern = '/\.(png|webp|jpg|jpeg)(\?.*)?$/i';
+    $pattern = '/\.(png|webp|jpg|jpeg|gif)(\?.*)?$/i';
     // URLに正規表現がマッチすればtrueを返す
     return preg_match($pattern, $url);
 }
 
-function save_with_webp($img_path){
+function save_with_webp($img_path, $url){
+    if (str_starts_with($img_path, "./")){
+        if(!str_ends_with($url, "/")){
+            $url = $url . "/";
+            $img_path = str_replace("./", $url, $img_path);
+        }
+        else{
+            $img_path = str_replace("./", $url, $img_path);
+        }
+    }
     $data = file_get_contents($img_path);
     $tmp_filename = mt_rand(1, 100000000);
 
@@ -46,6 +55,7 @@ function save_with_webp($img_path){
     $pattern_png = '/\.(png)(\?.*)?$/i';
     $pattern_jpg = '/\.(jpg|jpeg)(\?.*)?$/i';
     $pattern_webp = '/\.(webp)(\?.*)?$/i';
+    $pattern_gif = '/\.(gif)(\?.*)?$/i';
 
     if(preg_match($pattern_jpg, $img_path)){
         // jpgファイルを読み込む
@@ -54,7 +64,6 @@ function save_with_webp($img_path){
     else if(preg_match($pattern_png, $img_path)){
         // jpgファイルを読み込む
         $image_file = imagecreatefrompng("tmp/" . $tmp_filename);
-
     }
     else{
         return ($img_path);
@@ -159,5 +168,129 @@ function getSpecificLineFromFile($filename, $lineNum) {
    
     return trim($targetLine);
 }
+
+function isDomainUrlonList($url){
+    $domain_list = ['ja.wikipedia.org', 'www.youtube.com', 'www.google.com'];
+
+    foreach($domain_list as $value){
+        $url_parts = parse_url($url, PHP_URL_HOST);
+        if ($url_parts == $value){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function Readability_Raw_Extra($url){
+    $domain_list = ['ja.wikipedia.org', 'www.youtube.com', 'www.google.com'];
+
+
+    $url_parts = parse_url($url, PHP_URL_HOST);
+    if ($url_parts == $domain_list[0]){
+        //$content = "
+        //<form action=\"read.php\" method=\"get\" class=\"search_container\">
+            
+        //    <input type=\"text\" name=\"extra\" value=\"true\" hidden>
+        //    <input type=\"text\" name=\"url\" value=\"read.php?url=https://ja.wikipedia.org/wiki/\" hidden>
+        //    <input type=\"text\" class=\"search_box_input\" placeholder=\"Wikipedia内を検索...\" name=\"titles\">
+        //</form>";
+
+
+        $url_parts_all = parse_url($url);
+        $titles = explode('/', $url_parts_all['path'])[2];
+
+        // 検索用URL
+        $tmp_url = "https://ja.wikipedia.org/w/api.php?";
+
+        $params_list = array('action'=>'query','format'=>'json','prop'=>'extracts','explaintext'=>'True','exsectionformat'=>'plain', 'titles'=>$titles);
+        $req_param = http_build_query($params_list);
+        $request = $tmp_url.$req_param;
+
+        $json = file_get_contents($request,true);
+        $json_d = json_decode($json,true);
+
+        $article_id = key(array_slice( $json_d['query']['pages'], 0, 1, true));
+
+        if($article_id != -1){
+            extract_substrings('--', '-', str_replace("\n", '-', $json_d['query']['pages'][$article_id]['extract']));
+
+            $content_with_bar = str_replace("\n", '-', $json_d['query']['pages'][$article_id]['extract']);
+
+            //$array_content_with_bar_h4 = extract_substrings('---', '--', $content_with_bar);
+
+            //foreach($array_content_with_bar_h4 as $value){
+              //  $content_with_bar = str_replace("---{$value}--", "<br><h4>{$value}</h4>", $content_with_bar);
+            //}
+
+            $array_content_with_bar_h3 = extract_substrings('---', '-', $content_with_bar);
+
+            foreach($array_content_with_bar_h3 as $value){
+                $content_with_bar = str_replace("---{$value}-", "<br><h3>{$value}</h3>", $content_with_bar);
+            }
+
+            $content_with_bar = str_replace("-", "<br>", $content_with_bar);
+
+            $title = $titles;
+            $content_raw = '<p>' . $content_with_bar . '</p>';
+
+            return array($title, $content_raw);
+        }
+        else{
+            $title = $titles;
+            $content_raw = "ウィキペディアに{$titles}という項目が存在しませんでした。";
+            return array($title, $content_raw);
+        }
+    }
+    else if($url_parts == $domain_list[1]){
+        $title = "Youtubeには対応していません。";
+        $content_raw = "「元ページを表示」をクリックしてYoutubeアプリでご視聴ください。";
+        return array($title, $content_raw);
+    }
+    else if($url_parts == $domain_list[2]){
+        $title = "Google.comには対応していません。";
+        $content_raw = "本サイトはGoogle検索を使用していますので検索結果は同じです。軽量化を望まれていて、Google検索を使用したい場合はこのサービスに付属している検索エンジンをご利用ください。";
+        return array($title, $content_raw);
+    }
+    
+
+    return false;
+}
+
+function iterative_cutter($needle_start, $needle_end, $content)
+{
+  $start = 0;
+  $results = array();
+  while(true){
+    $ret = strpos($content, $needle_start, $start);
+    if($ret == false){
+      return $results;
+    }
+    $start = $ret + strlen($needle_start);
+    $end = strpos($content, $needle_end, $start);
+    $line = substr($content, $start, $end - $start);
+    $results[] = $line;
+    $start = $end + strlen($needle_end);
+  }
+}
+
+function extract_substrings($begin, $end, $text)
+{
+    $pos = 0;
+    $result = array();
+    while(true){
+        $found = strpos($text, $begin, $pos);
+        if($found == false){
+            return $result;
+        }
+
+        $pos = $found + strlen($begin);
+        $stop = strpos($text, $end, $pos);
+        $sub = substr($text, $pos, $stop - $pos);
+        $result[] = $sub;
+        $pos = $stop + strlen($end);
+    }
+}
+
 
 ?>
--- read.php
+++ read.php
@@ -4,28 +4,57 @@
 require_once('components/xss.php');
 require_once('components/components.php');
 
-$url = $_REQUEST['url'];
+if(isset($_REQUEST['url'])){
+    $url = $_REQUEST['url'];
 
-//画像ファイル化によって対応を分ける
-if(is_image_url($url)){
-    header('Content-Type: image/webp');
-    $imagefile_path = save_with_webp($url);
-    readfile($imagefile_path);
-    unlink($imagefile_path);
+    //画像ファイル化によって対応を分ける
+    if(is_image_url($url)){
+        $root_url = $_REQUEST['root_url'];
+
+        header('Content-Type: image/webp');
+
+        $imagefile_path = save_with_webp($url, $root_url);
+        readfile($imagefile_path);
+        unlink($imagefile_path);
+    }
+    else{
+        if(isDomainUrlonList($url)){
+            if (isset($_REQUEST['extra'])){
+                $url = $url . $_REQUEST['titles'];
+            }
+
+            $content_raw_array = Readability_Raw_Extra($url);
+
+            $title = $content_raw_array[0];
+            $content_raw = $content_raw_array[1];
+
+            //HTMLのリンクを変換
+            $content_url_prefix = Add_Prefix_2_Link($content_raw, "http://localhost/sakusaku-net/read.php?url=");
+
+            //HTMLのリンクを変換
+            $content_url_prefix = Add_Prefix_2_Img_Src($content_url_prefix, "http://localhost/sakusaku-net/read.php?root_url={$url}&url=");
+        }
+        else{
+            //Rawの本文を取得
+            $content_raw_array = Readability_Raw($url);
+
+            $title = $content_raw_array[0];
+            $content_raw = $content_raw_array[1];
+
+            //HTMLのリンクを変換
+            $content_url_prefix = Add_Prefix_2_Link($content_raw, "http://localhost/sakusaku-net/read.php?url=");
+
+            //HTMLのリンクを変換
+            $content_url_prefix = Add_Prefix_2_Img_Src($content_url_prefix, "http://localhost/sakusaku-net/read.php?root_url={$url}&url=");
+        }
+    }
 }
 else{
-    //Rawの本文を取得
-    $content_raw_array = Readability_Raw($url);
-
-    $title = $content_raw_array[0];
-    $content_raw = $content_raw_array[1];
-
-    //HTMLのリンクを変換
-    $content_url_prefix = Add_Prefix_2_Link($content_raw, "http://localhost/sakusaku-net/read.php?url=");
-
-    //HTMLのリンクを変換
-    $content_url_prefix = Add_Prefix_2_Img_Src($content_url_prefix, "http://localhost/sakusaku-net/read.php?url=");
+    $title = "URLを指定してください";
+    $content_url_prefix = "URLが指定されていません。URLを指定して再度試してください。";
+    $url = "read.php";
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -42,8 +71,8 @@ else{
 <body>
     <header>
         <h1><a href="index.php"></p><span style="color: red;">サクサク</span><span  class="logo_title_net">ネット</span></a></h1>
-        <form action="search.php" method="get" class="search_container">
-            <input type="text" class="search_box_input" placeholder="URL..." name="keyword">
+        <form action="read.php" method="get" class="search_container">
+            <input type="text" class="search_box_input" placeholder="URL..." name="url">
         </form>
         <a href="<?php echo $url?>">元ページを表示</a>
     </header>
